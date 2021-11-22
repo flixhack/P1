48f6ab7c0ca4cda4c3ef10c2c9c119a44a0e1956
Nicolai Olsen <mariaermeganice@gmail.com>
2021-11-22 13:23:09 +0100

    Co-authored-by: xGreenaa <xGreenaa@users.noreply.github.com> Co-authored-by: Toooody <Toooody@users.noreply.github.com> Co-authored-by: nyny123321 <nyny123321@users.noreply.github.com> Co-authored-by: IronicJuice <IronicJuice@users.noreply.github.com> Co-authored-by: Greathius <Greathius@users.noreply.github.com>

--- calendar.c
+++ calendar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void accessCalendar (){
     int c;
@@ -10,4 +11,37 @@ void accessCalendar (){
             putchar(c);
         fclose(file);
     }
+}
+
+//shamelessly stolen from https://stackoverflow.com/questions/29429384/search-string-in-a-file-in-c
+
+void accessDate (){
+        int num =0;
+        char word[2000];
+        char string[50];
+        char calendar[100] = {0};
+
+        while(calendar[0]!= '0')
+        {
+                FILE *in_file = fopen("calendar.txt", "r");
+                if (in_file == NULL)
+                {
+                        printf("Error file missing\n");
+                        exit(-1);
+                }
+
+                printf("please enter a word(enter 0 to end)\n");
+                scanf("%s", calendar);
+                while ( fscanf(in_file,"%s", string) == 1)
+                {
+                        //Add a for loop till strstr(string, student) does-not returns null. 
+                        if(strstr(string, calendar)!=0) {//if match found
+                                num++;
+                        }
+                }
+                printf("we found the word %s in the file %d times\n", calendar ,num );
+                num = 0;
+                fclose(in_file);
+        }
+        return 0;
 }
