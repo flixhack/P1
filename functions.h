5790b0527f5463bc196c4da2d0d729bf770db33d
Marcus Lau <m@lau.it>
2021-11-26 11:52:34 +0100

    Moved functions to functions.h

---
+++ functions.h
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void printSchedule (int userID, int loginID){
+
+}
+
+void writeLog (char string[], int currentDate, int affectedDate){
+
+}
+
+int login(void){
+    char loginScan;
+
+    printf("\nEnter your username (T, S, A): ");
+    scanf("%c", &loginScan);
+    switch (loginScan){
+    case 's':
+    case 'S':
+        loggedIn = 1;
+        break;
+    case 't':
+    case 'T':
+        loggedIn = 2;
+        break;
+    case 'a':
+    case 'A':
+        loggedIn = 3;
+        break;
+    default:
+        break;
+    }
+    printf("\nYou are logged in as %d", loggedIn );
+    return(loggedIn);
+}
+
+void readyState(int loginVar){
+    char command;
+    while (loggedIn != 0) {
+        printf("\nEnter command here: ");
+        scanf(" %c", &command);
+        findCommand(command);
+
+    }
+}
+
+void findCommand(char command){
+    switch (command){
+    case 'a':
+    case 'A':
+        assignments();
+        break;
+    case 's':
+    case 'S':
+        schedule();
+        break;
+    case 'l':
+    case 'L':
+        logOut();
+     default:
+        break;
+    }
+}
+
+
+void logOut(){
+  loggedIn = 0;
+
+}
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "functions.h"
 // #include "calendar.h"
-int loggedIn = 0;
+int loggedIn = 0; //needs to go
 
 void readyState(int),
      findCommand(char),
@@ -22,68 +23,3 @@ int main(void){
     return(0);
 }
 
-int login(void){
-    char loginScan;
-
-    printf("\nEnter your username (T, S, A): ");
-    scanf("%c", &loginScan);
-    switch (loginScan){
-    case 's':
-    case 'S':
-        loggedIn = 1;
-        break;
-    case 't':
-    case 'T':
-        loggedIn = 2;
-        break;
-    case 'a':
-    case 'A':
-        loggedIn = 3;
-        break;
-    default:
-        break;
-    }
-    printf("\nYou are logged in as %d", loggedIn );
-    return(loggedIn);
-}
-
-void readyState(int loginVar){
-    char command;
-    while (loggedIn != 0) {
-        printf("\nEnter command here: ");
-        scanf(" %c", &command);
-        findCommand(command);
-
-    }
-}
-
-void findCommand(char command){
-    switch (command){
-    case 'a':
-    case 'A':
-        assignments();
-        break;
-    case 's':
-    case 'S':
-        schedule();
-        break;
-    case 'l':
-    case 'L':
-        logOut();
-     default:
-        break;
-    }
-}
-
-void assignments(){
-
-}
-
-void schedule(){
-
-}
-
-void logOut(){
-  loggedIn = 0;
-
-}
