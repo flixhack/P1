7318504184d3a069da9358d106254f4fd6450c22
Marcus Lau <m@lau.it>
2021-11-26 12:50:19 +0100

    Co-authored-by: Toooody <Toooody@users.noreply.github.com> Co-authored-by: Greathius <Greathius@users.noreply.github.com>

--- functions.h
+++ functions.h
@@ -15,7 +15,48 @@ void printSchedule (int userID, int loginID){
 
 }
 
-void writeLog (char string[], int currentDate, int affectedDate){
+int readTime(void){
+    int n1 = 0;
+    int n2 = 2;
+    char col;
+    scanf(" %i", &n1);
+    scanf("%c", &col);
+    if (col != ":"){
+        return 0;
+    }
+    else {
+        scanf("%i", &n2);
+    }
+    if (n1 > 24 || n1 < 0){
+        return 0;
+    }
+    if (n2 > 60 || n2 < 0){
+        return 0;
+    }
+    // God fucking help me
+    /* if (n2 < 10 && (n2 != 09 || n2 != 08 || n2 != 07 || n2 != 06 || n2 != 05 || n2 != 04 || n2 != 03 || n2 != 02 || n2 != 01 || n2 != 00){
+        return 0;
+    } */
+    
+}
+
+const char * recieveString(){
+    int i = 0;
+
+
+
+    char string[] = (char*)malloc(i * sizeof(char));
+    free(string[]);
+
+    while (scanf(" %c",&string[i])){
+        i++;
+    }
+
+
+    return string[];
+}
+
+void writeLog (int currentDate, int affectedDate){
 
 }
 
