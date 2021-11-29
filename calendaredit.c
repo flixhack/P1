4de31b7a040f51cc2e3a2e20df3ef142848154c9
xGreenaa <94369120+xGreenaa@users.noreply.github.com>
2021-11-29 11:25:34 +0100

    Created calendaredit, which now does the tasks of adding, deleting and replacing lines in the calendar database. Calendarreplace is obsolete

---
+++ calendaredit.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_LENGTH 100
+
+//Defining structures, since they aren't included in any header file yet.
+typedef struct date {
+    int week,
+        day,
+        month,
+        year;
+    double hoursFree;
+} date;
+
+typedef struct module {
+    date date1;
+    int startTime;
+    int duration;
+    char subject[100];
+} module;
+
+int main() {
+
+    FILE *readFile = fopen("calendar.txt", "r");
+    FILE *writeTemp = fopen("replace.tmp", "w");
+    char buffer[MAX_LINE_LENGTH];
+    char newLine[MAX_LINE_LENGTH];
+    int line, count = 0, i;  
+    char editDate[MAX_LINE_LENGTH];
+    char editMonth[2];
+    char editYear[4];
+    char penis[2] = {'\n', '\0'};
+    char mode = 'E';
+
+    //Give warning if unable to read calendar.txt
+    if (readFile == NULL) {
+        printf("Unable to open database.");
+        exit(EXIT_FAILURE);
+    }
+
+    //Defining date1 and module1 for testing purposes
+    date date1;
+
+    date1.day = 3;
+    date1.month = 2;
+    date1.year = 2012;
+
+    module module1;
+    module1.startTime = 1130;
+    module1.duration = 60;
+
+    strcpy(module1.subject, "chemistry");
+
+    printf("Month = %i\n", date1.month);
+
+    printf("What mode do you want to use? A/D/R: ");
+    scanf("%c", &mode);
+
+    if (mode == 'D') {
+        printf("Enter the line you want to delete: ");
+        scanf("%i", &line);
+    }
+    else if (mode == 'A' || mode == 'R') {
+        printf("Enter the line you want to edit: ");
+        scanf("%i", &line);
+
+        printf("Enter the new line: ");
+        scanf("%s", &newLine);
+
+        strcat(newLine, penis);
+    }
+
+    //Reads through each line of the db, copies content to the temp file, and makes the requested change once it's location is found
+    while ((fgets(buffer, MAX_LINE_LENGTH, readFile)) != NULL) {
+        count++;
+
+        if (count == line) {
+            if (mode == 'D') {
+                fputs("", writeTemp);
+            }
+            else if (mode == 'A') {
+                strcat(buffer, newLine);
+                fputs(buffer, writeTemp);
+            }
+            else if (mode == 'R') {
+                fputs(newLine, writeTemp);
+            }
+        }
+        else {
+            fputs(buffer, writeTemp);
+        }
+    }
+
+    fclose(readFile);
+    fclose(writeTemp);
+
+    //Replaces the old db file with the one that now has the desired corrections
+    remove("calendar.txt");
+    rename("replace.tmp", "calendar.txt");
+
+    return 0;
+}
