fb2f41bf5fd71814c017546d86ad7c0e3ab6c997
Nicolai Olsen <mariaermeganice@gmail.com>
2021-12-02 11:02:16 +0100

    basic structure for schedule and assignment

--- functions.h
+++ functions.h
@@ -4,17 +4,13 @@
 
 void readyState(int),
      findCommand(char),
-     assignments(),
-     schedule(),
+     assignments(userID, loginID),
+     schedule(userID, loginID),
      logOut();
 
 int login();
 
 int loggedIn = 0;
-
-void printSchedule (int userID, int loginID){
-
-}
 
 void writeLog (char string[], int currentDate, int affectedDate){
 
@@ -59,11 +55,11 @@ void findCommand(char command){
     switch (command){
     case 'a':
     case 'A':
-        assignments();
+        assignments(userID, loginID);
         break;
     case 's':
     case 'S':
-        schedule();
+        schedule(userID, loginID);
         break;
     case 'l':
     case 'L':
@@ -73,12 +69,125 @@ void findCommand(char command){
     }
 }
 
-void assignments(/* arguments */) {
-  /* code */
+void assignments(int userID, int loginID) {
+  int run = 1;
+  char command;
+  while (run == 1){
+    if (loginID == 2 || loginID == 3){
+      printf("Do you wish to see current assignments, go to the assignment editor, or go back? (C/E/B)");
+      scanf("%c", &command);
+      switch (command){
+        case 'c':
+        case 'C':
+        printAssignments(userID, loginID);
+        break;
+        case 'e':
+        case 'E':
+        assignmentEditor(userID, loginID);
+        break;
+        case 'b':
+        case 'B':
+        run = 0;
+        break;
+      }
+    }
+    else printAssignments(userID, 1);
+  }
 }
 
-void schedule(/* arguments */) {
-  /* code */
+void printAssignments(int userID, int LoginID){
+
+}
+
+void assignmentEditor(int userID, int LoginID){
+  int run = 1;
+  char command;
+  while (run == 1){
+    printf("Create an assignment, edit an assignment, delete an assignment or go back? (C/E/D/B)");
+    scanf("%c", &command);
+    switch (command)
+    {
+    case 'c':
+    case 'C':
+      
+      break;
+    case 'e':
+    case 'E':
+
+      break;
+    case 'd':
+    case 'D':
+
+      break;
+    case 'b':
+    case 'B':
+      run = 0;
+      break;
+    default:
+        break;
+    }  
+  }
+}
+
+void schedule(int userID, int loginID) {
+  int run = 1;
+  char command;
+  while (run == 1){
+    if (loginID == 3){
+      printf("Do you wish to see the schedule, go to the schedule editor, or go back? (C/E/B)");
+      scanf("%c", &command);
+      switch (command){
+        case 'c':
+        case 'C':
+        printSchedule(userID, loginID);
+        break;
+        case 'e':
+        case 'E':
+        scheduleEditor(userID);
+        break;
+        case 'b':
+        case 'B':
+        run = 0;
+        break;
+      }
+    }
+    else printSchedule(userID, 1);
+  }
+}
+
+void printSchedule (int userID, int loginID){
+
+}
+
+void scheduleEditor(int userID){
+  int run = 1;
+  char command;
+  while (run == 1){
+    printf("Create a module, Edit a module, delete a module or go back? (C/E/D/B)");
+    scanf("%c", &command);
+    switch (command)
+    {
+    case 'c':
+    case 'C':
+      
+      break;
+    case 'e':
+    case 'E':
+
+      break;
+    case 'd':
+    case 'D':
+      
+      
+      break;
+    case 'b':
+    case 'B':
+      run = 0;
+      break;
+    default:
+    
+    }  
+  }
 }
 
 void logOut(){
