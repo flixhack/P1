84d3108b1593091fce800be83564b919b32cdf89
Marcus Lau <m@lau.it>
2021-12-06 12:42:56 +0100

    tried.

--- structs.h
+++ structs.h
@@ -1,17 +1,17 @@
-struct date{
+typedef struct date{
     int week,
         day,
         month,
         year;
     double hoursFree;
-};
-typedef struct date date;
+} date;
 
-struct element{
+
+typedef struct element{
     date date;
     int time;   //determines start- or end-time. 1930 = 19:30
     int duration;    //duration in minutes
     char *subject; 
     int type; //1: Module, 2: Assignment, 3: Homework, 4: Test
-};
-typedef struct element element;
+} element;
+
