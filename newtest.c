d3c3feaf479028ac0a95c0683e012e2e405cb9b1
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2021-12-14 16:03:58 +0100

    Pushed newtest.c

---
+++ newtest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+//#include "C:/Users/m/Desktop/P1/P1/structs.h"
+#include "calendar.h"
+//#include "C:/Users/m/Desktop/P1/P1/functions.h"
+//#include "C:/Users/m/Desktop/P1/P1/dbManager.h"
+
+
+
+
+int main(void){
+  // element assignment;
+  // assignment.startDate.day = 1;
+  // assignment.startDate.month = 2;
+  // assignment.startDate.year = 2012;
+  // assignment.endDate.day = 25;
+  // assignment.endDate.month = 2;
+  // assignment.endDate.year = 2012;
+  date date;
+  date = scanForEarliestAssignmentDate();
+  printf("Earliest assignment was given on: %i/%i/%i\n", date.day, date.month, date.year);
+  return 0;
+}
