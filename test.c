10fef62f50a0dfe9b09c20dc06e6dacea40bcb7f
flixhack <felix.benjamin.mathiassen@gmail.com>
2021-12-14 16:38:56 +0100

    test

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include "dbManager.h"
 
 date stringToDateConvert(char);
-
+void readSection(int locOne, int locTwo, char tempDB[][100], char databaseSelect[]);
 
 
 int main() {
@@ -20,8 +20,44 @@ int main() {
   if (readFile == NULL) {
     printf("Database file not found. Contact an administrator.");
     exit(EXIT_FAILURE);
+
+    readSection(&locOne, &locTwo, char tempDB[][100], char databaseSelect[]);
+
+
+
   }
 
+
+  void readSection(int locOne, int locTwo, char tempDB[][100], char databaseSelect[]) {
+      //If statement uses the location variables assigned earlier (locOne and locTwo) to only store the necesarry text
+      int lineCount = countLines(char *calendar.txt);
+      int bytes = 0;
+      char string[MAX_LINE_LENGTH];
+
+      FILE *readFile = fopen(databaseSelect, "r");
+      if (readFile == NULL){
+          printf("Database file not found. Contact an administrator\n");
+          exit(EXIT_FAILURE);
+      }
+
+      bytes = locOne;
+      int i = 0;
+      while (fscanf(readFile, "%s", string) == 1) {
+          if (lineCount > bytes && lineCount < locTwo) {
+              strcpy(tempDB[i], string);
+              i++;
+              bytes++;
+              // if (string[strlen(string) - 1] != '\n') {
+              //     printf("\n");
+              // }
+          }
+          lineCount++;
+          printf("string");
+      }
+      fclose(readFile);
+  }
+
+  stringToDateConvert(char string[], char separator);
   currentDate = date;
 
   while(fscanf(databaseSelect, "%s", str) != EOF && *currentDate != lastestDate){
