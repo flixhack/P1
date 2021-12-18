f3ae5423c5a9635992ea8a48dd91c03f7aa22cad
IronicJuice <m@lau.it>
2021-12-18 16:24:45 +0100

    for Ian <3 Merry Christmas!

--- dbManager.h
+++ dbManager.h
@@ -132,10 +132,10 @@ void readSection(int locOne, int locTwo, char tempDB[][MAX_LINE_LENGTH], char da
 void calendarSplit (char tempDB[][100], int lineLoc, char entryTime[][MAX_LINE_LENGTH], char entryDuration[][MAX_LINE_LENGTH], char entryType[][4], char entrySubject[][MAX_LINE_LENGTH], char endDate[][10]) {
     int parseSwitch = 1, k;
 
-    memset(entryTime[lineLoc], '\0', MAX_LINE_LENGTH);
+    //memset(entryTime[lineLoc], '\0', MAX_LINE_LENGTH);
     memset(entryDuration[lineLoc], '\0', MAX_LINE_LENGTH);
-    memset(entryType[lineLoc], '\0', MAX_LINE_LENGTH);
-    memset(endDate[lineLoc], '\0', MAX_LINE_LENGTH);
+    //memset(entryType[lineLoc], '\0', MAX_LINE_LENGTH);
+    //memset(endDate[lineLoc], '\0', MAX_LINE_LENGTH);
 
     printf("tempDB[0]: %s\n", tempDB[0]);
 
