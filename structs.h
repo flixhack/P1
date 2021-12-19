00f279e72d24a846ece4dc4d293a9d79f768d73b
xGreenaa <94369120+xGreenaa@users.noreply.github.com>
2021-12-19 06:56:43 +0100

    Made a patchwork solution for structs.h. Also there's now a printf in calendar.h. Big deal.

--- structs.h
+++ structs.h
@@ -9,8 +9,8 @@ typedef struct date{
 typedef struct element{
     date startDate;
     date endDate;
-    char *time;   //determines start- or end-time. 
+    char time[100];   //determines start- or end-time. 
     int duration;    //duration in minutes
-    char *subject; 
+    char subject[100]; 
     int type; //1: Module, 2: Assignment, 3: Homework, 4: Test
 } element;
